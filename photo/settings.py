405cb412c7bc290cb02059108066cd1f1d83a6a5
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-28 10:57:16 +0300

    style:config app in settings

--- photo/settings.py
+++ photo/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tilda'
+    'tilda.apps.TildaConfig'
 ]
 
 MIDDLEWARE = [
