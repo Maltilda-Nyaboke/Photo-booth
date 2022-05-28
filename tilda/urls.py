c2217c84a26199a127d74699f48d00cd36a9f412
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-28 12:41:55 +0300

    feature:create view photo path

--- tilda/urls.py
+++ tilda/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('add.html', views.add, name='add')
+    path('add.html', views.add, name='add'),
+    path('view.html', views.view, name='view')
     
 ]
