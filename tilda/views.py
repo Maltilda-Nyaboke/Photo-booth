7e2126069aeda460c7740d7f9e32683f20b4fa9e
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-29 05:18:18 +0300

    style:function for viewing all photos

--- tilda/views.py
+++ tilda/views.py
@@ -1,10 +1,15 @@
+from multiprocessing import context
 from django.shortcuts import render
 from .models import Category,Location,Image
 
 
 # Create your views here.
 def home(request):
-    return render (request, 'photos/index.html')
+    categories = Category.objects.all()
+    context = {
+        'categories': categories,
+    }
+    return render (request, 'photos/index.html',context)
 
 def add(request):
     return render (request, 'photos/add.html')    
