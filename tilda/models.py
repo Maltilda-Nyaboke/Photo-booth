93cac0e58002106faa84d58e9b02e29bbc31c9e4
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-29 03:21:15 +0300

    style:add location model

--- tilda/models.py
+++ tilda/models.py
@@ -11,3 +11,12 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Location(models.Model):
+    name = models.CharField(max_length=255,null=False,blank=False)
+
+
+    def __str__(self):
+        return self.name
+
+
+
