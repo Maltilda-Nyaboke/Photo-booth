5e7035e67002a6e8b5edf874714de5bd92b73602
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-29 03:21:56 +0300

    style:add image model

--- tilda/models.py
+++ tilda/models.py
@@ -18,5 +18,14 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+class Image(models.Model):
+    image = models.ImageField(null=False, blank=False)
+    name = models.CharField(max_length=30,blank=False,null=False)
+    description = models.CharField(max_length=255,null=False,blank=False)
+    location = models.ForeignKey(Location, null=False, blank=False)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.description
 
 
