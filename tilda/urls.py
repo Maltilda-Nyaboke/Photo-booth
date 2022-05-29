9fabc06ca363401c968bb925aeab309909f9deb2
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-29 14:49:19 +0300

    add navigation bar

--- tilda/urls.py
+++ tilda/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add, name='add'),
-    path('view/', views.view, name='view')
+    path('view/<int:id>', views.view, name='view')
     
 ]
