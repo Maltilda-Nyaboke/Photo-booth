1b44ee73bcc55d50e89e377a2837654f3106cfa7
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-30 08:04:54 +0300

    feature:write view function for category

--- tilda/views.py
+++ tilda/views.py
@@ -37,14 +37,25 @@ def view(request,id):
     return render (request, 'photos/view.html',{'image':image})
 
 
-def search_image(request):
-    if 'image' in request.GET and request.GET["image"]:
-        search_term = request.GET.get("image")
-        searched_images = Image.search_by_category(search_term)
+def search_results(request):
+
+    if 'name' in request.GET and request.GET["name"]:
+        search_term = request.GET.get("name")
+        searched_images = Image.search_image(search_term)
+        print(searched_images)
         message = f"{search_term}"
 
-        return render(request, 'photos/search.html',{"message":message,"image": searched_images})
+        return render(request, 'search.html',{"message":message,"images": searched_images})
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'photos/search.html',{"message":message})    
+        return render(request, 'search.html',{"message":message})
+
+def get_category(request,category_id):
+    images=Image.filter_by_category(category_id)
+
+    return render (request,'index.html',{'images':images})
+def get_location(request,location_id):
+    images=Image.filter_by_location(location_id)
+
+    return render (request,'index.html',{'images':images})        
