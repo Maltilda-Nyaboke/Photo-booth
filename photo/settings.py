3596611173089f678150e97814d3503f7bcb8832
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-31 12:12:05 +0300

    test:deployment

--- photo/settings.py
+++ photo/settings.py
@@ -148,7 +148,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/static/')
+    os.path.join(BASE_DIR, 'static')
 ]
 # MEDIA_ROOT = BASE_DIR / 'static/media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
